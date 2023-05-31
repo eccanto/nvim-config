a8cd393cfe597b5abd638788b69eaabb095074f8
eccanto <ccanto.erik@gmail.com>
2023-05-31 19:35:34 -0300

    refactor: add coc-sh lsp

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ sudo ln -s -f ${NVIM_CONFIG} /root/.config/nvim
 # setting coc package
 echo -e "${BOLDGREEN}configuring coc...${ENDCOLOR}"
 cp ./resources/coc-settings.json ~/.config/nvim/coc-settings.json
-nvim +'CocInstall coc-json coc-tsserver coc-pyright coc-yaml'
+nvim +'CocInstall coc-json coc-tsserver coc-pyright coc-yaml coc-sh'
 
 # end
 echo -e "${BOLDGREEN}\nfinished.${ENDCOLOR}\n"
