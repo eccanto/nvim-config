33c262563458522462c76233fd7b8a552b74c531
Erik Ccanto <ccanto.erik@gmail.com>
2023-09-30 16:34:56 -0300

    refactor: remove unnecesary coc package

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ sudo ln -s -f ${NVIM_CONFIG} /root/.config/nvim
 # setting coc package
 echo -e "${BOLDGREEN}configuring coc...${ENDCOLOR}"
 cp ./resources/coc-settings.json ~/.config/nvim/coc-settings.json
-nvim +'CocInstall coc-json coc-tsserver coc-pyright coc-yaml coc-sh coc-highlight coc-pairs coc-clangd coc-sh'
+nvim +'CocInstall coc-json coc-tsserver coc-pyright coc-yaml coc-sh coc-highlight coc-pairs coc-clangd'
 
 # end
 echo -e "${BOLDGREEN}\nfinished.${ENDCOLOR}\n"
